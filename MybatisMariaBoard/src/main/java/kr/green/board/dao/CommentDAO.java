b9fbd2368760a3aab353e4c96406849e417def44
CHOI DONG OH <chlehddh8062@gmail.com>
2022-02-15 02:25:23 +0900

    220119 MabatisMariaBoard 댓글형게시판

---
+++ MybatisMariaBoard/src/main/java/kr/green/board/dao/CommentDAO.java
@@ -0,0 +1,25 @@
+package kr.green.board.dao;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import kr.green.board.vo.CommentVO;
+
+public interface CommentDAO {
+	// <!-- 1. ? 번글의 댓글이 ? 개인지를 세야 한다 -->
+	int selectCount(SqlSession sqlSession, int ref);
+	// <!-- 2. ? 번글의 댓글 목록 -->
+	List<CommentVO> selectList(SqlSession sqlSession, int ref);
+	// <!-- 3. 저장 -->
+	void insert(SqlSession sqlSession, CommentVO commentVO);
+	// <!-- 4. 수정 -->
+	void update(SqlSession sqlSession, CommentVO commentVO);
+	// <!-- 5. 삭제 -->
+	void delete(SqlSession sqlSession, int idx);
+	// <!-- 6. ? 글의 댓글 모두 삭제 -->
+	void deleteByRef(SqlSession sqlSession, int ref);
+	// <!-- 7. 비번 검사 -->
+	int passwordCheck(SqlSession sqlSession, HashMap<String, String> map);
+}
