ebfb34439a381e2d0c03a63a41f49d703a82149c
CHOI DONG OH <chlehddh8062@gmail.com>
2022-02-15 02:30:25 +0900

    220125 FileBoardMaria 파일업로드게시판

---
+++ FileBoardMaria/src/main/java/kr/green/file/vo/BoardFileVO.java
@@ -0,0 +1,22 @@
+package kr.green.file.vo;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import lombok.Data;
+
+/*
+ * 	create table boardFile(
+	idx int primary key auto_increment,
+	ref int not null,
+	oriName varchar(100) not null,
+	saveName varchar(100) not null,
+	);
+ */
+@Data
+@XmlRootElement
+public class BoardFileVO {
+	private int idx;
+	private int ref;
+	private String oriName;
+	private String saveName;
+}
